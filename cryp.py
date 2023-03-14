f7868f2fdc87a7988a451a54734e7cd1ca0f9682
Frygidairus <94869660+Frygidairus@users.noreply.github.com>
2023-03-14 09:12:07 +0100

    Update cryp.py

--- cryp.py
+++ cryp.py
@@ -3,9 +3,11 @@ import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+# Here you can edit the path to the salt
 with open('salt.txt', 'rb') as file:
     salt = file.read()
 
+# Set up the Key Derivation Function
 kdf = PBKDF2HMAC(
     algorithm=hashlib.sha256(),
     length=32,
@@ -13,13 +15,14 @@ kdf = PBKDF2HMAC(
     iterations=480000
 )
 
+# From a password, derive a key and store a a hash of it in the myKey.txt file (used during the initialization)
 def create_and_store_key(pwd):
     key = base64.urlsafe_b64encode(kdf.derive(pwd))
     print(key)
     with open('myKey.txt', 'wb') as file:
         file.write(hashlib.sha256(key).digest())
 
-
+# Check wether a password matches the password set up during the initialization
 def check_key(ukey):
     uhash = hashlib.sha256(ukey).digest()
 
@@ -34,10 +37,8 @@ def check_key(ukey):
 def derive_key(pwd):
     key = base64.urlsafe_b64encode(kdf.derive(pwd))
     return key
-# key = create_and_store_key()
-# print(key)
 
-
+# Encrypt and decrypt a specific file thanks to the provided key
 def encrypt_file(key, nameOfFile):
     with open(nameOfFile, 'rb') as file:
         original = file.read()
